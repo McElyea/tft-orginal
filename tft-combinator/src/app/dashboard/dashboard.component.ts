c4b48f734138baf00e5c19201c59849e61982028
Jon McElyea <jonmcelyea@gmail.com>
2020-02-28 10:37:51 -0700

    another step to combinations

--- tft-combinator/src/app/dashboard/dashboard.component.ts
+++ tft-combinator/src/app/dashboard/dashboard.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Item } from '../item';
 import { ItemService } from '../item.service';
-
+import { isNullOrUndefined } from 'util';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: [ './dashboard.component.css' ]
+  styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
   allItems: Item[] = [];
   basicItems: Item[] = [];
   collectedItems: Item[] = [];
   craftableItems: Item[] = [];
+  itemCombinations: Item[][] = [];
 
   constructor(private itemService: ItemService) { }
 
@@ -21,71 +22,183 @@ export class DashboardComponent implements OnInit {
 
   getItems(): void {
     this.allItems = this.itemService.getItems();
-    for(let i = 1; i<= 9; i++){
+    for (let i = 1; i <= 9; i++) {
       this.basicItems.push(this.allItems.find(item => item.id === i));
-    }      
-      
+    }
+
   }
 
-  addCollectedItem(item: Item): void { 
-      this.collectedItems.push(item);
-      this.updateCraftableItems();  
+  addCollectedItem(item: Item): void {
+    this.collectedItems.push(item);
+    this.updateCraftableItems();
+  }
+
+  addCraftedItem(item: Item): void {
+    this.collectedItems.push(item);
+    let itemFirstDigit = Math.floor(item.id / 10);
+    let itemSecondDigit = item.id % 10;
+    let newItem1 = this.getBasicItemById(itemFirstDigit);
+    let newItem2 = this.getBasicItemById(itemSecondDigit);
+    this.removeCollectedItem(newItem1);
+    this.removeCollectedItem(newItem2);
   }
 
   removeCollectedItem(item: Item): void {
     let index = this.collectedItems.indexOf(item);
-    if(item.id > 9){      
+    if (item.id > 9) {
       let itemFirstDigit = Math.floor(item.id / 10);
-      let itemSecondDigit = item.id % 10;      
-      let newItem1 = this.basicItems[itemFirstDigit-1];
-      let newItem2 = this.basicItems[itemSecondDigit - 1];
+      let itemSecondDigit = item.id % 10;
+      let newItem1 = this.getBasicItemById(itemFirstDigit);
+      let newItem2 = this.getBasicItemById(itemSecondDigit);
       this.collectedItems.push(newItem1);
       this.collectedItems.push(newItem2);
     }
-    this.collectedItems.splice(index,1); 
-    this.updateCraftableItems(); 
+    this.collectedItems.splice(index, 1);
+    this.updateCraftableItems();
   }
 
   updateCraftableItems(): void {
-    let collectedItemIds: number[] = [];
-    for(let i = 0; i < this.collectedItems.length; i++){
-      collectedItemIds.push(this.collectedItems[i].id);
-    }
-    
-    this.craftableItems = [];
-    let max: number = collectedItemIds.length; 
-    if(max >= 2){ 
-      for(let i = 0; i < max; i++){
-        for(let j = i+1; j < max; j++){
-          let firstId = collectedItemIds[i];
-          let secondId = collectedItemIds[j];
-          if(firstId > 9 || secondId > 9){
-            continue;
-          }
-          if(firstId > secondId)
-          {
-            let tempId = secondId;
-            secondId = firstId;
-            firstId = tempId;   
-          }
-          let newItemId = firstId * 10 + secondId;
-          if(!this.craftableItems.find(item => item.id === newItemId)){
-            let newItem = this.allItems.find(item => item.id === newItemId);
-            this.craftableItems.push(newItem);
-          }
-        }
-      }      
-    }
-    
+
+    this.craftableItems = this.getUniqueCraftableItems(this.collectedItems);
+    this.updateCombinations();
   }
 
-  addSelectedCraftedItem(item: Item): void {
-    this.collectedItems.push(item);    
-    let itemFirstDigit = Math.floor(item.id / 10);
-    let itemSecondDigit = item.id % 10;      
-    let newItem1 = this.basicItems[itemFirstDigit-1];
-    let newItem2 = this.basicItems[itemSecondDigit - 1];
-    this.removeCollectedItem(newItem1);
-    this.removeCollectedItem(newItem2);
+  getUniqueCraftableItems(items: Item[]): Item[] {
+    let uniqueCraftableItems: Item[] = [];
+    let itemIds: number[] = [];
+    for (let i = 0; i < items.length; i++) {
+      itemIds.push(items[i].id);
+    }
+
+    let max: number = itemIds.length;
+    if (max >= 2) {
+      for (let i = 0; i < max; i++) {
+        for (let j = i + 1; j < max; j++) {
+          let firstId = itemIds[i];
+          let secondId = itemIds[j];
+          if (firstId > 9 || secondId > 9) {
+            continue;
+          }
+          let newItemId = this.getCraftedItemId(firstId, secondId)
+          if (!uniqueCraftableItems.find(item => item.id === newItemId)) {            
+            uniqueCraftableItems.push(this.findItemById(newItemId));
+          }
+        }
+      }
+    }
+    return uniqueCraftableItems;
   }
+  findItemById(itemId: number): Item{
+    return this.allItems.find(item => item.id === itemId);
+  }
+  updateCombinations(): void {
+    this.itemCombinations = this.updateListCombinations(this.collectedItems);
+  }
+
+  updateListCombinations(collectedItemsList: Item[]): Item[][] {    
+    if (collectedItemsList.length < 2) {
+      return [];
+    }
+    let collectedBasicItems: number[] = [];
+    let collectedCraftedItems: number[] = [];
+    for (let i = 0; i < collectedItemsList.length; i++) {
+      let thisItemId = collectedItemsList[i].id;
+      if (thisItemId > 9) {
+        collectedCraftedItems.push(thisItemId);
+      }
+      else {
+        collectedBasicItems.push(thisItemId);
+      }
+    }
+
+    if (collectedBasicItems.length < 2) {
+      return [];
+    }
+
+    let remainingCombinations: Item[][] = this.getCombinations(collectedBasicItems, this.craftableItems);
+    for(let i = 0; i < remainingCombinations.length;i++){
+      if(remainingCombinations[i].length > 3){
+        let newCombos = this.updateListCombinations(remainingCombinations[i]);
+        let craftedItems = [];
+        for(let j= 0; j< newCombos.length;j++){
+          if(newCombos[j].length > 3){
+
+          }
+        }
+      }
+      else{
+
+      }
+    
+    }
+    return remainingCombinations;      
+  }
+
+  private getCombinations(collectedBasicItems: number[], craftableItems: Item[]): Item[][] {
+    let currentCraftableList = cloneArray(craftableItems);
+    let currentCraftableListLength = currentCraftableList.length;
+    let combinations: Item[][] = [];
+    for (let i = 0; i < currentCraftableListLength; i++) {
+      let newItem = currentCraftableList[i];
+      let itemFirstDigit = Math.floor(newItem.id / 10);
+      let itemSecondDigit = newItem.id % 10;
+      let filteredBasicItemList = this.removeCraftingItems(collectedBasicItems, itemFirstDigit, itemSecondDigit);
+      let newItemlist: Item[] = [];
+      newItemlist.push(newItem);
+      
+      for (let j = 0; j < filteredBasicItemList.length; j++) {
+        let remainingCraftableItems = this.getUniqueCraftableItems(filteredBasicItemList[j]);
+        
+        let newBasicItemToBePushed = this.getBasicItemById(filteredBasicItemList[j]);
+        newItemlist.push(newBasicItemToBePushed);
+      }
+      combinations.push(newItemlist);
+    }
+    return combinations;
+  }
+
+
+  removeCraftingItems(collectedBasicItems: number[], itemFirstDigit: number, itemSecondDigit: number) {
+    let tempBasicItemList = cloneArray(collectedBasicItems);
+    let index1 = tempBasicItemList.indexOf(this.getBasicItemById(itemFirstDigit).id);
+    tempBasicItemList.splice(index1, 1);
+    let index2 = tempBasicItemList.indexOf(this.getBasicItemById(itemSecondDigit).id);
+    tempBasicItemList.splice(index2, 1);
+    return tempBasicItemList;
+  }
+
+  continueCombinations(): void {
+    let currentCombinationsLength = this.itemCombinations.length;
+    for(let i = 0; i < currentCombinationsLength; i++){
+
+    }
+  }
+
+  getBasicItemById(id: number): Item {
+    return this.basicItems[id - 1];
+  }
+
+  getCraftedItemId(firstId: number, secondId: number): number {     
+    if (firstId > secondId) {
+      let tempId = secondId;
+      secondId = firstId;
+      firstId = tempId;
+    }
+    let newItemId = firstId * 10 + secondId;
+    return newItemId;
+  }
+
+}
+function removeItemFromList(list: any[], itemToRemove: any) {
+  let newList = cloneArray(list);
+  let id = newList.findIndex(itemToRemove);
+  return list.splice(id,1);
+}
+
+function cloneArray(list: any[]) {
+  let tempList = [];
+  for (let x = 0; x < list.length; x++) {
+    tempList.push(list[x]);
+  }
+  return tempList;
 }
